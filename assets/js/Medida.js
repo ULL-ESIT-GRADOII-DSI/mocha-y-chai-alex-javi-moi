adb131ba1a0516522cb1b25fe2c674ddca453fa7
alu0100505009 <alu0100505009@ull.edu.es>
2016-03-14 10:39:03 +0000

    Actualizado fichero Medida.js

--- assets/js/Medida.js
+++ assets/js/Medida.js
@@ -1,62 +1,69 @@
 (function (exports) {
     "use strict";
 
-    var regexp = new XRegExp();
-
-    function Medida(valor, tipo) {
-        /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
+    function Medida(valor, tipo) {      
+        /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */ 
         /* ademas de new Medida(45.2, "Km") */
-        this.valor = valor;
-        this.tipo = tipo;
-    }
-
-    function Temperatura(valor, tipo) {
-        Medida.call(this, valor, tipo);
-    }
-
-    Temperatura.prototype = new Medida();
-    exports.Temperatura = Temperatura;
-
-    exports.convertir = function () {
-        var valor = document.getElementById('convert').value,
-        elemento = document.getElementById('converted');
-
-        valor = valor.toLowerCase();
-        valor = XRegExp.exec(valor, regexp);
-        if (valor) {
-            var numero = valor.cantidad,
-                tipo = valor.medida[0],
-                tipo_dest = valor.medida_dest[0];
-
-            numero = parseFloat(numero);
-            console.log("Valor: " + numero + ", Tipo: " + tipo + ", Destino: " + tipo_dest);
-
-            var obj_numer = null;
-            switch (tipo) {
-                case 'c':
-                    obj_numer = new Celsius(numero);
-                break;
-                case 'f':
-                    obj_numer = new Fahrenheit(numero);
-                break;
-                case 'k':
-                    obj_numer = new Kelvin(numero);
-                break;
-            }
-            switch (tipo_dest) {
-                case 'c':
-                    elemento.innerHTML = obj_numer.toCelsius().toFixed(2) + " Celsius";
-                break;
-                case 'f':
-                    elemento.innerHTML = obj_numer.toFahrenheit().toFixed(2) + " Fahrenheit";
-                break;
-                case 'k':
-                    elemento.innerHTML = obj_numer.toKelvin().toFixed(2) + " Kelvin";
-                break;
-            }
+   
+        
+          if (!tipo) {  
+            var match = XRegExp.exec(valor, regexp);  
+            console.log(match);
+            var _valor = match.numero;
+            var _tipo = match.tipo;
+            
         } else {
-            console.error('Error en la expresión');
-            elemento.innerHTML = "Error en la entrada";
+            var _valor = valor;
+            var _tipo = tipo;
         }
+        
+        //return _valor,_tipo;
+        this.getValor = function() { return _valor; };
+        this.getTipo = function() { return _tipo; };
+        
+      
     };
+
+
+    Medida.match = function(entrada)
+    {   
+        var regexp = XRegExp('(?<numero> [+-]?\\d+(\\.\\d+)?([e][+-]?\\d+)?[ ]*) # valor \n' + 
+							               '(?<tipo> [fck])          # tipo de entrada   \n' + 
+							               '(?<to> \\s*(?:to)?\\s*)  # to opcional \n' +
+							               '(?<destino> [fck])       # tipo destino', 'x');
+       
+        return XRegExp.exec(entrada, regexp);
+    }
+
+
+
+    Medida.convertir = function(valor) {
+      
+      var measures = Medida.measures;
+      var match = Medida.match(valor);
+      
+      if (match) {
+        var numero = match.numero,
+            tipo   = match.tipo,
+            destino = match.destino;
+            console.log("numero: " + numero);
+            console.log("tipo: " + tipo);
+            console.log("destino: " + destino);
+    
+        try {
+          var source = new measures[tipo](numero);          // new Fahrenheit(32)
+          var target = "to"+measures[destino].name;         // "toCelsius"
+          return source[target]().toFixed(2) + " "+target;  // "0 Celsius"
+        }
+        catch(err) {
+          return 'Desconozco como convertir desde "'+tipo+'" hasta "'+destino+'"';
+        }
+      }
+      else
+        return "Introduzca una temperatura valida: 330e-1 F to C";
+    };
+
+
+    exports.Medida = Medida;
+   
 })(this);
