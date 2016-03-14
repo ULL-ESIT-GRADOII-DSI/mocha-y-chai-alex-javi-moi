82b729f6fd2bee44bc240ca561a0ef38a44959c6
alu0100782851 <alu0100782851@ull.edu.es>
2016-03-14 17:10:20 +0000

    Añadiendo cambios en fichero distancia

--- assets/js/Distancia.js
+++ assets/js/Distancia.js
@@ -8,7 +8,7 @@
     Distancia.prototype = Object.create(Medida.prototype);
     Distancia.prototype.constructor= Distancia;
   
-    
+ /*   
     Kilometros.prototype = Object.create(Distancia.prototype);
     Kilometros.prototype.constructor = Kilometros;
    
@@ -37,14 +37,49 @@
       var converted = (this.getValor()* 1.6);
       return converted;
     }
+ */  
+   
+    Metros.prototype = Object.create(Distancia.prototype);
+    Metros.prototype.constructor = Metros;
+   
+  
+    function Metros(valor) {
+      Distancia.call(this, valor, "m");
+      this.name = "Metros";
+    }
+    
+    Metros.prototype.toPulgadas = function () {
+      var converted = (this.getValor() *39.3701);
+      return converted;
+    }
+    
+    
+    Pulgadas.prototype = Object.create(Distancia.prototype);
+    Pulgadas.prototype.constructor = Pulgadas;
+   
+  
+    function Pulgadas(valor) {
+      Distancia.call(this, valor, "p");
+      this.name = "Pulgadas";
+    }
+    
+    Pulgadas.prototype.toMetros = function () {
+      var converted = (this.getValor()* 0.0254);
+      return converted;
+    }
+   
    
    Medida.measures = {
-      "km" : Kilometros,
-      "mi" : Millas
+     // "km" : Kilometros,
+     // "mi" : Millas,
+      "m" : Metros,
+      "p" : Pulgadas
     };
 
-    exports.Distancia = Distancia;
-    exports.Kilometros = Kilometros;
-    exports.Millas = Millas;
+    //exports.Kilometros = Kilometros;
+    //exports.Millas = Millas;
+    exports.Metros = Metros;
+    exports.Pulgadas = Pulgadas;
+    
     
 })(this);
