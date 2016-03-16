558e7ac744bf16524ab39ff81824166edd61fda9
alu0100505009 <alu0100505009@ull.edu.es>
2016-03-16 16:15:36 +0000

    Actualizado gulpfile y dependencias

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,37 @@ var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
 
 gulp.task('minify', function () {
-  gulp.src('temperature.js')
+  gulp.src('main.js')
   .pipe(uglify())
   .pipe(gulp.dest('minified'));
+  gulp.src('Medida.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+  gulp.src('Temperatura.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+  gulp.src('Distancia.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+  gulp.src('Moneda.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+  
 
   gulp.src('./index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('./minified/'))
 
-  gulp.src('./*.css')
+  gulp.src('./custom.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+  gulp.src('./global.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+  gulp.src('./normalize.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+  gulp.src('./skeleton.css')
    .pipe(minifyCSS({keepBreaks:true}))
    .pipe(gulp.dest('./minified/'))
 });
