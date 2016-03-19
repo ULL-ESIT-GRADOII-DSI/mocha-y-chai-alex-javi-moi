4f91be44cd35df1af24b438fb64b1db6569e8a87
alu0100767421 <alu0100767421@ull.edu.es>
2016-03-19 17:22:51 +0000

    Añadida temperatura Rankine

--- assets/js/Temperatura.js
+++ assets/js/Temperatura.js
@@ -26,6 +26,11 @@
     Fahrenheit.prototype.toKelvin = function () {
       return ((this.val - 32) / (9/5)) + 273.15;
     };
+    //Fahrenheit to Rankine
+    Fahrenheit.prototype.toRankine = function () {
+      return (this.val + 459.67);
+    };
+  
   
 
     /********************METODOS CELSIUS********************/
@@ -46,7 +51,10 @@
     Celsius.prototype.toKelvin = function () {
       return (this.val + 273.15);
     };
-
+    //Celsius to Rankine
+    Celsius.prototype.toRankine = function () {
+      return (((9/5)*this.val)+491.67);
+    };
 
     
     /******************METODOS KELVIN**********************/
@@ -67,12 +75,38 @@
     Kelvin.prototype.toFahrenheit = function () {
       return (((this.val - 273.15) * 9/5) + 32);
     };
+    //Kelvin to Rankine
+    Kelvin.prototype.toRankine = function () {
+      return (this.val*(9/5));
+    };
     
+     /******************METODOS Rankine**********************/
+    function Rankine(valor) {
+      Temperatura.call(this, valor, "r");
+    }
     
+    Rankine.prototype = new Temperatura();
+    Rankine.prototype.constructor = Rankine;
+    Medida.measures.r = Rankine;  
+        
+    //Rankine to celsius
+    Rankine.prototype.toCelsius = function () {
+      return ((5/9)*(this.val-491.67));
+    };
+    
+    //Rankine to fahrenheit
+    Rankine.prototype.toFahrenheit = function () {
+      return (this.val - 459.67);
+    };
+    //Rankine to Kelvin
+     Rankine.prototype.toKelvin = function () {
+      return (this.val*(5/9));
+    };
     /* ******exports********* */
     exports.Temperatura = Temperatura;
     exports.Fahrenheit = Fahrenheit;
     exports.Celsius = Celsius;
     exports.Kelvin = Kelvin;
+    exports.Rankine= Rankine;
 
 })(this);
